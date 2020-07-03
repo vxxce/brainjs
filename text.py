5a2a99f800faf1d5af445c1d881b34d6b5331c82
Zachary Olpin <info@zacharyolp.in>
2020-07-03 08:23:24 -0600

    const fs = require('fs');
    // beware, synchronous
    let getDirContents = path => {
      let files = []
      const dir = fs.opendirSync(path)
      while (true) {
        let d = dir.readSync()
        if (d == null) break;
        else {
          if (d.name.trim().endsWith("e"))
          files.push(d.name)
        }
        return files
      }
    
      let collate = async (file, collation) => {
          let data = fs.readFileSync(file)
          fs.appendFileSync(collation, data.toString())
        }
      }
    
      let main = async () => {
        let files = await getDirContents(path.join(__dirname, 'dump/'))
        files.forEach(f => {
          collate(f, __dirname)
        })
      }
    
    main()

---
+++ text.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+# Simple, possible memory probs with sufficient bytes
+with open('collated.txt', 'w') as f:
+	  for dirEnt in [e for e in os.listdir(os.getcwd()) if e.endswith(".txt") and not e.startswith(".")]:
+        with open(dirEnt, 'r') as d:
+            for l in d.readlines():
+                f.write(l + "\n")
+
+
+# Or with very large individual files, use pandas:
+def collate(args*):
+    collated = pd.DataFrame()
+    for file in args:
+        chunked = pd.read_csv(file, "\n", chunksize=10000)
+        chunks = [c for c in chunked]
+        df = pd.concat(chunks)
+        collated.append(df)
+    return collated
+
