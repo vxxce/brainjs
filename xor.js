f0c1a278143a05a79b91306239d097ca3d0714a6
Zachary Olpin <info@zacharyolp.in>
2020-07-02 23:25:20 -0600

    xor

---
+++ xor.js
@@ -0,0 +1,14 @@
+const brain = require('brain.js')
+
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
+
+const trainingData = [
+  {input: [1, 1], output: [0]},
+  {input: [1, 0], output: [1]},
+  {input: [0, 0], output: [0]},
+  {input: [0, 1], output: [1]},
+]
+
+net.train(trainingData)
+
+console.log(net.run([1, 0]))
